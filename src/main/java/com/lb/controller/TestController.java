dc1e7ecfb3b9de3b97d2c213b7979db2410303b5
liint biao <biao.liint@cn.mcd.com>
2019-12-23 14:15:09 +0800

    2019年12月23日14:15:08

--- src/main/java/com/lb/controller/TestController.java
+++ src/main/java/com/lb/controller/TestController.java
@@ -1,8 +1,17 @@
 package com.lb.controller;
 
 import com.alibaba.fastjson.JSONArray;
+import com.lb.server.WxServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author libioa
@@ -12,8 +21,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @RequestMapping("/")
-    public String IndexController(){
-        String sql="select * from Student";
-        return JSONArray.toJSONString(sql);
+    public String IndexController() {
+        String sql = "select * from Student";
+        Map<String, ArrayList> map = new HashMap<String, ArrayList>();
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("1");
+        arrayList.add("2");
+        map.put("userId", arrayList);
+        return JSONArray.toJSONString(map);
+    }
+
+    @RequestMapping("/Wx")
+    /**
+     *@Description 校验
+     *@Param
+     *@Return
+     *@Author libiao
+     *@Date
+     */
+    public void wxIndex(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        //校验请求
+        if (WxServer.check(timestamp, nonce, signature) != false) {
+            PrintWriter writer = response.getWriter();
+            //原样返回
+            writer.print(echostr);
+            //System.out.println("校验成功");
+            writer.flush();
+            writer.close();
+        } else {
+            System.out.println("校验失败");
+        }
     }
 }
---
+++ src/main/java/com/lb/server/WxServer.java
@@ -0,0 +1,57 @@
+package com.lb.server;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+/*
+ * @Author 李彪
+ * @Description
+ * @Date 9:36 2019/12/23
+ * @return
+ **/
+public class WxServer {
+    private static final String TOKEN = "123abc";
+
+    /**
+     *@Description 校验
+     *@Param
+     *@Return
+     *@Author libiao
+     *@Date
+     */
+    /**
+     *
+     * @param timestamp
+     * @param nonce
+     * @param signature
+     * @return
+     */
+    public static boolean check(String timestamp, String nonce, String signature) {
+        String[] strs = new String[]{TOKEN, timestamp, nonce};
+        Arrays.sort(strs);
+        String str = strs[0] + strs[1] + strs[2];
+        String mysign = sha1(str);
+        return mysign.equalsIgnoreCase(signature);
+    }
+
+    private static String sha1(String str) {
+        try {
+            //获取加密对象
+            MessageDigest md = MessageDigest.getInstance("sha1");
+            //加密
+            byte[] digest = md.digest(str.getBytes());
+            char[] chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+            StringBuilder sb = new StringBuilder();
+            //处理加密结束
+            for (byte b : digest) {
+                sb.append(chars[(b >> 4) & 15]);
+                sb.append(chars[b & 15]);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
